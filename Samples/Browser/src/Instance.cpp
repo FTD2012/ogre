6a592dd33ca47d36fe420ac25b22b288e0f6c7c9
lvjiaming <962831141@qq.com>
2019-02-19 11:43:47 +0800

    feat: 向场景中添加光源，模型，阴影，complete 'Lights, Cameras, and Shadows'

--- Samples/Browser/src/Instance.cpp
+++ Samples/Browser/src/Instance.cpp
@@ -16,46 +16,74 @@ void Instance::setup()
     Ogre::RTShader::ShaderGenerator *shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
     shadergen->addSceneManager(scnMgr);
     
-    // set ambient light
-    scnMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+    // ambient light
+    scnMgr->setAmbientLight(Ogre::ColourValue(0, 0, 0));
+    scnMgr->setShadowTechnique(Ogre::ShadowTechnique::SHADOWTYPE_STENCIL_MODULATIVE);
     
-    // add named light
-    Ogre::Light *light = scnMgr->createLight("MainLight");
-    Ogre::SceneNode *lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-    lightNode->attachObject(light);
-    lightNode->setPosition(20, 80, 50);
-    
-    // add camera
+    // camera
     Ogre::Camera *cam = scnMgr->createCamera("myCam");
     cam->setNearClipDistance(5);
-    cam->setAutoAspectRatio(true);
+    
+    // camera scene node
     Ogre::SceneNode *camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(200, 300, 400);
+    camNode->lookAt(Ogre::Vector3(0, 0, 0), Ogre::Node::TransformSpace::TS_WORLD);
     camNode->attachObject(cam);
-    camNode->setPosition(0, 0, 140);
-    getRenderWindow()->addViewport(cam);
     
-    // add first entity with default position
-    Ogre::Entity *ogreEntity = scnMgr->createEntity("ogrehead.mesh");
-    Ogre::SceneNode *ogreNode = scnMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(0, 0, 0));
-    ogreNode->attachObject(ogreEntity);
+    // view port
+    Ogre::Viewport *vp = getRenderWindow()->addViewport(cam);
+    cam->setAspectRatio(Ogre::Real(vp->getActualWidth()) / Ogre::Real(vp->getActualHeight()));
     
-    // add second entity with position
-    Ogre::Entity *ogreEntity2 = scnMgr->createEntity("ogrehead.mesh");
-    Ogre::SceneNode *ogreNode2 = scnMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(84, 48, 0));
-    ogreNode2->attachObject(ogreEntity2);
+    // ninja entity
+    Ogre::Entity *ninjaEntity = scnMgr->createEntity("ninja.mesh");
+    ninjaEntity->setCastShadows(true);
+    scnMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ninjaEntity);
     
-    // add third entity with scale
-    Ogre::Entity *ogreEntity3 = scnMgr->createEntity("ogrehead.mesh");
-    Ogre::SceneNode *ogreNode3 = scnMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(0, 104, 0));
-    ogreNode3->setScale(2, 1.2, 1);
-    ogreNode3->attachObject(ogreEntity3);
+    // create plane mesh manually
+    Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+    Ogre::MeshManager::getSingletonPtr()->createPlane("ground", Ogre::RGN_DEFAULT, plane, 1500, 1500, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
     
-    // add forth entity with rotation
-    Ogre::Entity *ogreEntity4 = scnMgr->createEntity("ogrehead.mesh");
-    Ogre::SceneNode *ogreNode4 = scnMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(-84, 48, 0));
-    ogreNode4->roll(Ogre::Degree(-90));
-    ogreNode4->attachObject(ogreEntity4);
+    // create ground entity with plane mesh
+    Ogre::Entity *groundEntity = scnMgr->createEntity("ground");
+    groundEntity->setMaterialName("Examples/Rockwall");
+    groundEntity->setCastShadows(false);
+    scnMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
     
-    camNode->setPosition(0, 47, 222);
+    // add spot light
+    Ogre::Light *spotLight = scnMgr->createLight("SpotLight");
+    spotLight->setDiffuseColour(0, 0, 1.0);
+    spotLight->setSpecularColour(0, 0, 1.0);
+    spotLight->setType(Ogre::Light::LT_SPOTLIGHT);
+    spotLight->setSpotlightRange(Ogre::Degree(35), Ogre::Degree(50));
+    
+    Ogre::SceneNode *spotNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    spotNode->setDirection(-1, -1, 0);
+    spotNode->setPosition(Ogre::Vector3(200, 200, 0));
+    spotNode->attachObject(spotLight);
+    
+    
+    // add direction light
+    Ogre::Light *directionLight = scnMgr->createLight("DirectionLight");
+    directionLight->setType(Ogre::Light::LT_DIRECTIONAL);
+    directionLight->setDiffuseColour(Ogre::ColourValue(0.4, 0, 0));
+    directionLight->setSpecularColour(Ogre::ColourValue(0.4, 0, 0));
+    
+    Ogre::SceneNode *directionNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    directionNode->setDirection(0, -1, 1);
+    directionNode->attachObject(directionLight);
+    
+    // add point light
+    Ogre::Light *pointLight = scnMgr->createLight("PointLight");
+    pointLight->setType(Ogre::Light::LT_POINT);
+    pointLight->setDiffuseColour(0.3, 0.3, 0.3);
+    pointLight->setSpecularColour(0.3, 0.3, 0.3);
+    
+    Ogre::SceneNode *pointNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    pointNode->setPosition(Ogre::Vector3(0, 150, 250));
+    pointNode->attachObject(pointLight);
+    
+    
+    
+    
     
 }
